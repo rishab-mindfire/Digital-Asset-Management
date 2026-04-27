70fd7e5e1ccc91832c6915c457f38cb54bc03a1a
rishab raj verma <rishabrajv@mindfiresolutions.com>
2026-04-28 00:23:48 +0530

    feat : add spiner, loader, test setup in frontend

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3001,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/setup.js',
+  },
+
+  build: {
+    outDir: 'dist',
+    manifest: true,
+  },
+
+  base: '/',
+});
