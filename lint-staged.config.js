416721f4d4436ca085f347a8c4fd6880ec89d936
rishab raj verma <rishabrajv@mindfiresolutions.com>
2026-05-07 18:43:00 +0530

    fix: husky setup

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,10 @@
 export default {
   'frontend/**/*.{ts,tsx}': [
-    'npm --prefix Frontend run lint',
-    'npm --prefix Frontend run format'
+    'npm --prefix frontend run lint',
+    'npm --prefix frontend run format'
   ],
   'backend/**/*.{ts,tsx}': [
-    'npm --prefix Backend run lint',
-    'npm --prefix Backend run format'
+    'npm --prefix backend run lint',
+    'npm --prefix backend run format'
   ],
 };
